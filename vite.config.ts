df5073a6c170b5b350c847d3a2152727e4cb821e
HimanshuYadav1811 <himanshuyadav181995@gmail.com>
2026-03-20 12:41:54 +0530

    tailwind stable and redux install

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'   // or vue(), svelte(), etc.
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
+
