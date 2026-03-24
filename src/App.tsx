dd0f7b7fe96966aef1df8e3f000f84e69794f51d
rupesh <102rupeshsharma@gmail.com>
2026-03-24 11:06:45 +0530

    added reset password page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home/Index"
 import { Route, Routes } from "react-router-dom"
 import Testing from "./pages/Testing"
 import LoginPage from "./pages/login"
+import ResetPassword from "./pages/ResetPassword/Index"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/testing" element={<Testing />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
   )
